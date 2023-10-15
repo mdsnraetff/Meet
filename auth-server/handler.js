cca073ac010060ebbb5f0f7699f47a1bf2d87d59
Madison <96503969+mdsnraetff@users.noreply.github.com>
2023-10-15 17:52:25 -0400

    test + handler addition for submit

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -62,6 +62,53 @@ module.exports.getAuthURL = async () => {
       });
   };
 
+  //Get Cal. Events
+
+  module.exports.getCalendarEvents = async (event) => {
+    // Decode authorization code extracted from the URL query
+    const code = decodeURIComponent(`${event.pathParameters.access_token}`);
+
+    oAuth2Client.setCredentials({ access_token });
+
+    return new Promise((resolve, reject) => {
+
+      calendar.events.list(
+        {
+          calendarId: CALENDAR_ID,
+          auth: oAuth2Client,
+          timeMin: new Date().toISOString(),
+          singleEvents: true,
+          orderBy: "startTime",
+        },
+        (error, response) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(response);
+          }
+        }
+      );
+    })
+      .then((results) => {
+        // Respond with OAuth token 
+        return {
+          statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+          },
+          body: JSON.stringify({ events: results.data.items }),
+        };
+      })
+      .catch((error) => {
+        // Handle error
+        return {
+          statusCode: 500,
+          body: JSON.stringify(error),
+        };
+      });
+  };
+
   return {
     statusCode: 200,
     headers: {
