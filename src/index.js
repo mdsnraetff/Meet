2aad5a06a2b7dda6933468b6a3ddaa51d26264d4
Madison <96503969+mdsnraetff@users.noreply.github.com>
2023-12-11 15:26:34 -0500

    atatus setup

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+
+import * as atatus from 'atatus-spa';
+atatus.config('2c9d229efd1043f2b444c444f065840d').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -21,3 +25,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
